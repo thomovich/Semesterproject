664d77f7c5f91498e3658a31e10c0ce812528d59
freum <freum@DESKTOP-RUTK4IJ>
2020-10-28 13:03:22 +0100

    Changes to FXML

--- src/ViewModel/ViewModelFactory.java
+++ src/ViewModel/ViewModelFactory.java
@@ -6,18 +6,25 @@ public class ViewModelFactory {
 	 private FrontPageViewModel frontPageViewModel;
 	 private Chapter1ViewModel chapter1viewModel;
 	 private Chapter2ViewModel chapter2viewModel;
+	 private ChaptersViewModel chaptersviewModel;
+
 
 
 	 public ViewModelFactory (MathModel model) {
 		 frontPageViewModel = new FrontPageViewModel(model);
+		 chaptersviewModel = new ChaptersViewModel(model);
 		 chapter1viewModel = new Chapter1ViewModel(model);
 		 chapter2viewModel = new Chapter2ViewModel(model);
 
-
 		}
+	 
 	 public FrontPageViewModel getFrontPageViewModel() {
 			
 			return frontPageViewModel;
+		}
+	 public ChaptersViewModel getChaptersViewModel() {
+			
+			return chaptersviewModel;
 		}
 	 
 	 public Chapter1ViewModel getChapter1ViewModel() {
