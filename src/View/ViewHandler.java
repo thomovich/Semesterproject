261388dcd4aff3fca808d948871f3f34391d50c0
thomas <thomas@192.168.0.17>
2020-11-10 12:09:32 +0100

    Fixed jar filer

--- src/View/ViewHandler.java
+++ src/View/ViewHandler.java
@@ -24,7 +24,7 @@ public class ViewHandler {
 
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
-		startmayorgame();
+		startFrontpage();
 		currentScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 	}
 
