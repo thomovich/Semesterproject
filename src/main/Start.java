a7212b1b7cea2f699abf280901b313567375ceb7
Frederik <freumert@gmail.com>
2020-12-01 10:16:20 +0100

    sdf

--- src/main/Start.java
+++ src/main/Start.java
@@ -7,9 +7,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		Application.launch(MyApplication.class);
-		//Hej Frede
-		//hvis du kan se dette er du i det rigtige
-		//d.11/18/2020
+	//Tissemand
 	}
 
 }
