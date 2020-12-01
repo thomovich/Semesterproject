7921cf89c060e8c725da0b76fcb7c84c44488ddb
Thomas <flaskesvar@gmail.com>
2020-12-01 10:42:46 +0100

    Fixed fejl

--- src/Gamestarter/DuckGame.java
+++ src/Gamestarter/DuckGame.java
@@ -1,5 +1,31 @@
 package Gamestarter;
 
-public class DuckGame {
+import javafx.animation.AnimationTimer;
+import javafx.scene.Group;
+import javafx.stage.Stage;
 
+
+
+public class DuckGame {
+	private final int WIDTH = 1280;
+	private final int HEIGHT = 720;
+	private boolean running = true;
+	
+public void start (Stage primaryStage) {
+	Group root = new Group();
+	primaryStage.setTitle("Duck game");
+	primaryStage.setHeight(HEIGHT);
+	primaryStage.setWidth(WIDTH);
+	primaryStage.centerOnScreen();
+	primaryStage.show();
+	
+	AnimationTimer timer = new AnimationTimer() {
+		public void handle(long now) {
+			if(running) {
+				
+			}
+		}
+		
+	};
+}
 }
